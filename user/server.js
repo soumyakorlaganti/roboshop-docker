e59a86f68c5c800e07cd3e4457d24bae6a58b188
soumya <korlagani.soumya@gmail.com>
2026-04-16 14:35:23 +0530

    docker

--- user/server.js
+++ user/server.js
@@ -1,7 +1,7 @@
 const instana = require('@instana/collector');
 // init tracing
 // MUST be done before loading anything else!
-// some change
+// some change, another change
 instana({
     tracing: {
         enabled: true
